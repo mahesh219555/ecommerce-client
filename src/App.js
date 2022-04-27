bad0e944b705b4352ad4c2eb05b5c24ab20be3bd
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-04-27 00:05:59 -0400

    started working on navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import { Container } from '@mantine/core';
 
 function App() {
   return (
     <>
-    Hello world
+    <BrowserRouter>
+    <Container>
+    <NavBar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/*' element={<NotFound />} />
+    </Routes>
+    </Container>
+    </BrowserRouter>
     </>
   );
 }
 
-export default App;
+export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Header, Container, Group } from '@mantine/core';
 
 const NavBar = () => {
+
   return (
-	<div>NavBar</div>
+  <Header height={60} mb={100}>
+    <Container 
+    style={{ 
+      display: 'flex', 
+      justifyContent: 'space-between', 
+      alignItems: 'center', 
+      height: '100%' }}
+    >
+      MyStore
+      <Group spacing={5}>
+        <a
+        key={null}
+        href='/'
+        >
+          Home
+        </a>
+        <a
+        key={null}
+        href='/add-product'
+        >
+          Products
+        </a>
+        <a
+        key={null}
+        href='/'
+        >
+          Admin
+        </a>
+        <a
+        key={null}
+        href='/'
+        >
+          Login/Register
+        </a>
+      </Group>
+
+    </Container>
+  </Header>
   )
 }
 
