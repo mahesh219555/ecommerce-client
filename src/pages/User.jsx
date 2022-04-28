2f3d86363c2ac24bda911981ed2b423019b8c318
Austin Flatt <80080997+austindflatt@users.noreply.github.com>
2022-04-28 11:45:51 -0400

    modified User

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -6,22 +6,23 @@ import Transactions from '../components/Transactions'
 
 const User = () => {
   const { isFetching, dispatch } = useContext(AuthContext);
-  const admin = JSON.parse(localStorage.getItem('user')).isAdmin;
-  const name = JSON.parse(localStorage.getItem('user')).firstName;
+  // const admin = JSON.parse(localStorage.getItem('user')).isAdmin;
+  // const name = JSON.parse(localStorage.getItem('user')).firstName;
+  console.log(JSON.parse(localStorage.getItem('user')))
 
   return (
     <>
-    <Title order={2} style={{ marginBottom: '10px' }}>Welcome to your account, {name} 🤑</Title>
-    <Button type="Submit" variant="light" color="orange" size="sm" style={{ marginBottom: '20px', marginRight: '10px' }}>Edit Profile</Button>
+    <Title order={2} style={{ marginBottom: '10px' }}>Welcome to your account,  🤑</Title>
+    <Button type="Submit" variant="light" color="orange" size="sm" style={{ marginBottom: '20px', marginRight: '10px' }}>Edit Account</Button>
     <Button type="Submit" variant="light" size="sm" onClick={() => dispatch(logout())} disabled={isFetching} style={{ marginBottom: '20px' }}>Logout</Button>
-    {
+    {/* {
     admin ?
     <>
     <Title order={2} style={{ marginBottom: '10px' }}>Admin Dashboard</Title>
     <Button type="Submit" variant="light" color="green" size="sm">Add New Product</Button>
     </> :
     <Transactions />
-    }
+    } */}
     </>
   )
 }
