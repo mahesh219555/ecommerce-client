968978d2b74541730ab4ef64666ce7e649db8772
mahesh219555 <108210803+mahesh219555@users.noreply.github.com>
2022-06-29 01:17:12 +0530

    Update apiCalls.js

--- src/context/authContext/apiCalls.js
+++ src/context/authContext/apiCalls.js
@@ -4,7 +4,7 @@ import { loginFailure, loginStart, loginSuccess } from "./AuthActions";
 export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
-    const res = await axios.post('https://tranquil-brook-13044.herokuapp.com/api/auth/login', user);
+    const res = await axios.post('https://digitabay.herokuapp.com/', user);
     res.data.isAdmin && dispatch(loginSuccess(res.data));
     dispatch(loginSuccess(res.data));
   } catch (error) {
